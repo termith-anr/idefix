7d1de04ea7f22ea0f0402f211b098686ec7e4004
MatthD <matthias.dieudonne@gmail.com>
2014-09-22 15:28:19 +0200

    starting refoactoring , apply algo

--- routes/export-docs.js
+++ routes/export-docs.js
@@ -1,40 +1,39 @@
-/*jshint node:true, laxcomma:true */
 'use strict';
+
 var path = require('path')
     , basename = path.basename(__filename, '.js')
     , debug = require('debug')('castor:routes:' + basename)
     , util = require('util')
     , datamodel = require('datamodel')
     , pmongo = require('promised-mongo')
+    , struct = require('object-path')
+    , CSV = require('csv-string')
     ;
-var map = function () {
-    /* global emit */
-    for (var key in this) { emit(key, null); }
-};
-var reduce = function (key, values) {
-    return null;
-};
+
 module.exports = function(config) {
-    var coll = pmongo(config.get('connexionURI')).collection(config.get('collectionName')) ;
+    var coll = pmongo(config.get('connexionURI')).collection(config.get('collectionName'));
+
     return datamodel()
-        .declare('template', function(req, fill) {
-            fill(basename + '.html');
-        })
         .declare('site', function(req, fill) {
             fill({
                 title : config.get('title'),
                 description : config.get('description')
             });
         })
+        .declare('headers', function(req, fill) {
+            var headers = {};
+            headers['Content-Type'] = require('../helpers/format.js')(req.params.format);
+            if (req.params.format === 'zip') {
+                headers['Content-Disposition'] = 'attachment; filename="export.zip"';
+            }
+            fill(headers);
+        })
         .declare('page', function(req, fill) {
             fill({
-                title : 'Overview documents',
+                title : 'Export some fields',
                 description : null,
-                types : ['text/html', 'application/json']
+                types : ['text/csv', 'application/json']
             });
-        })
-        .declare('user', function(req, fill) {
-            fill(req.user ? req.user : {});
         })
         .declare('config', function(req, fill) {
             fill(config.get());
@@ -45,32 +44,57 @@ module.exports = function(config) {
         .declare('selector', function(req, fill) {
             fill({ state: { $nin: [ "deleted", "hidden" ] } });
         })
-        .append('totalItems', function(req, fill) {
-            coll.find().count().then(fill).catch(fill);
+        .declare('parameters', function(req, fill) {
+            if ( ! Array.isArray(req.query.field)) {
+                req.query.field = typeof req.query.field  === 'string' ? [req.query.field] : [];
+            }
+            fill({
+                fields : req.query.field.map(function(x) {return x.replace(/[^\w\._$]/g, '');}) || ['wid']
+                , format: req.params.format
+                , startPage: Number(req.query.page || 1)
+                , nPerPage: Number(req.query.count || config.get('itemsPerPage'))
+            });
         })
-        .append('fields', function(req, fill) {
-            var self = this;
-            var opts = {
-                out : {
-                    replace: basename + '_fields'
-                },
-                query: self.selector
-            };
-            coll.mapReduce(map, reduce, opts).then(function(newcoll) {
-                newcoll.find().toArray(function (err, res) {
-                        fill(err ? err : res);
-                    }
-                );
-            }).catch(fill);
+        .append('items', function(req, fill) {
+            coll.find().toArray().then(fill).catch(fill);
         })
         .transform(function(req, fill) {
-            var n = this;
-            n.fields = this.fields.map(function(e) { return e._id; });
+            var n = this, r = [];
+            r.push(n.parameters.fields);
+            this.items.each(function(e) {
+                if (n.parameters.fields.length > 0) {
+                    r.push(n.parameters.fields.map(function(x) {
+                            var y = struct.get(e, x);
+                            if (y === null || y === undefined) {
+                                return undefined;
+                            }
+                            else if (typeof y === 'string' ||  typeof y === 'number' || y instanceof Date ) {
+                                return y;
+                            }
+                            else {
+                                return undefined;
+                            }
+                        })
+                    );
+                }
+            });
+            n.items = r;
             fill(n);
         })
         .send(function(res, next) {
-            render(res, this, next);
-        }
-    )
+            var self = this;
+            if (self.page.types.indexOf(self.headers['Content-Type']) === -1) {
+                next();
+            }
+            else if (self.headers['Content-Type'] === 'text/csv') {
+                res.type(self.headers['Content-Type']).send(CSV.stringify(self.items)).end();
+            }
+            else if (self.headers['Content-Type'] === 'application/json') {
+                res.json(self.items);
+            }
+            else {
+                next();
+            }
+        })
         .takeout();
-};
+}
