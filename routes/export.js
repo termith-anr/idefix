7c3b308bff984493feb269a188f2ef52a8514d2a
MatthD <matthias.dieudonne@gmail.com>
2014-10-01 16:13:51 +0200

    Started export : header CSv + filter  notedKeywords + get word , method , title

--- routes/export.js
+++ routes/export.js
@@ -3,28 +3,57 @@ var CSV = require('csv-string'),
     pmongo = require('promised-mongo'),
     path = require('path'),
     basename = path.basename(__filename, '.js');
+    sugar = require('sugar');
 
 module.exports = function(config) {
 
     var coll = pmongo(config.get('connexionURI')).collection(config.get('collectionName'));
 
+    var csvDocs = [];
+
+
     return function (req, res) {
-        coll
+
+
+         coll
         .find({ notedKeywords : {$exists : true}})
         .toArray()
-        .then(function(doc) {
-            if (!doc) {
-                console.log('Pas Doc');
+        .then(function(docs) {
+            if (!docs) {
+                console.log('Bad Doc');
                 return;
             }
-            else {
-                    console.log('Doc :');
+            docs.forEach(function(entity, index){
+                var docTitle = (entity.content.json.TEI.teiHeader.fileDesc.titleStmt.title[0] != undefined) ? entity.content.json.TEI.teiHeader.fileDesc.titleStmt.title[0]['#text'] : entity.content.json.TEI.teiHeader.fileDesc.titleStmt.title['#text'];
+                var tempDArrayDoc = [];
 
-                    res.send(doc);
+                Object.keys(entity.notedKeywords ,function(methodName , valueMethod){
+                    if( methodName != 'inist-francis' && methodName != '"inist-pascal') {
+                        var method = methodName;
 
-            }
+                        Object.keys(valueMethod , function(word , wordValues){
+                            currentWord = word;
+                            tempDArrayDoc.push([docTitle , methodName , word]);
+                        })
+                    }
+
+                });
+                console.log(tempDArrayDoc);
+                console.log('---------------------------------');
+                console.log(CSV.stringify(tempDArrayDoc) , ';');
+
+            });
 
 
-         })
+            res.set({
+                 'Content-Type': 'text/csv',
+                 'Content-Disposition':'attachment; filename="export.csv"'
+            });
+            res.send(csvDocs);
+
+        });
+
     };
+
+
 };
