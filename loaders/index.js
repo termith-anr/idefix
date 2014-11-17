f14c01c1d43ac0b0ba29cc32e32ef3d7880d9514
MatthD <matthias.dieudonne@gmail.com>
2014-11-17 11:30:22 +0100

    Method word are now put at 2 score auto

--- loaders/index.js
+++ loaders/index.js
@@ -7,22 +7,82 @@ module.exports = function(options) {
     return function (input, submit) {
 
         if(input.content.json.TEI.teiHeader.profileDesc.textClass.keywords){
-            var arrayMethodsKeywords = [];
-            Object.keys((input.content.json.TEI.teiHeader.profileDesc.textClass.keywords) , function(methodName , valueMethod){
-                if( (valueMethod.scheme != 'inist-francis') && (valueMethod.scheme != 'inist-pascal') && (valueMethod.scheme != 'cc') && (valueMethod.scheme != 'author')) {
+            var arrayMethodsKeywords = [],
+                arrayInistKeywords = [];
+            Object.keys((input.content.json.TEI.teiHeader.profileDesc.textClass.keywords) , function(index , valueMethod){
+                if( (valueMethod.scheme != 'inist-francis') && (valueMethod.scheme != 'inist-pascal') && (valueMethod.scheme != 'cc') && (valueMethod.scheme != 'author') && (valueMethod['xml#lang'] == 'fr')) {
                      var interArrayMethodsKeywords = [];
 
                      Object.keys(valueMethod.term , function( wordNb , wordValue){
                          interArrayMethodsKeywords.push(wordValue['#text']);
                      });
 
-                    arrayMethodsKeywords.push(interArrayMethodsKeywords);
+                    arrayMethodsKeywords[valueMethod.scheme]=(interArrayMethodsKeywords);
 
-                    console.log('---------------------------');
                 }
 
+                else if( ((valueMethod.scheme == 'inist-francis') || (valueMethod.scheme == 'inist-pascal')) && valueMethod['xml#lang'] == 'fr') {
+
+                    Object.keys(valueMethod.term , function( wordNb , wordValue){
+                        arrayInistKeywords.push(wordValue['#text']);
+                    });
+
+                }
+
+
             });
-            console.log(arrayMethodsKeywords);
+
+            Object.keys(arrayMethodsKeywords , function( idArray , value){
+                var listOfMetKw = value;
+                Object.keys(listOfMetKw , function( idMtWord , valueW){
+                    var methodWord = valueW;
+                    Object.keys(arrayInistKeywords , function( idInist , valueInist){
+                        if(valueW.toUpperCase() == valueInist.toUpperCase()){
+
+                            //console.log( 'NOM :', idArray,  'methode : ' , valueW , ' -  Valeur Inist : ' , valueInist);
+
+                            // SI noted K n'existe PAS
+                            if((input.notedKeywords == undefined) || (input.notedKeywords == null)){
+
+                                input.notedKeywords = {};
+                                input.notedKeywords[idArray] = {};
+                                input.notedKeywords[idArray][methodWord] = {
+                                    "note": 2
+                                };
+
+                            }
+                            // SI noted K EXISTE
+                            else{
+
+                                // SI La methode N4EXISTE PAS
+                                if((input.notedKeywords[idArray] == undefined) || (input.notedKeywords[idArray] == null)) {
+                                    input.notedKeywords[idArray] = {};
+                                    input.notedKeywords[idArray][methodWord] = {
+                                        "note": 2
+                                    };
+                                }
+                                // SI La methode existe
+                                else{
+                                    input.notedKeywords[idArray][methodWord] = {
+                                        "note": 2
+                                    };
+                                }
+
+                            }
+
+
+
+
+                        }
+
+                    });
+                });
+            });
+
+            console.log(input.notedKeywords);
+
+
+
         }
         submit(null, input);
     }
