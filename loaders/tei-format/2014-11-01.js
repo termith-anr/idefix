cbc324999a138c9c11d58c6df17f3a75ffa4ceb5
MatthD <matthias.dieudonne@gmail.com>
2015-02-27 15:39:56 +0100

    Started new loader ph1

--- loaders/tei-format/2014-11-01.js
+++ loaders/tei-format/2014-11-01.js
@@ -10,19 +10,66 @@ var objectPath = require('object-path'),
 'use strict';
 module.exports = function(options, config) {
     options = options || {};
+    config  = config.get();
     return function (input, submit) {
-        //if(config.keywordsSilencePath && config.keywordsEvalPath) {
 
-            // XML path with dot notation for silence/ pertinence
-            var silencePath     = "content.json." + config.silencePath,
-                pertinencePath  = "content.json." + options.pertinencePath;
+        /**
+         *  Infos show any kind of informations in console about the configuration
+         * @param err STRING the message to show
+         * @param type STRING (error , warning , info , valid)
+         * @param option STRING the option concerned
+         */
+        var infos = function(err,type,option) {
+            switch(type) {
+                case "error":
+                    console.error(kuler("ERROR - Config fails on option : " + option + " - Error is : \n" + err, "#e67e22"));
+                    break;
 
+                case "warning":
+                    console.warn(kuler("WARNING - Config problem on option : " + option + " , warning is : \n" + err, "#f39c12"));
+                    break;
 
-        //}
+                case "info":
+                    console.info(kuler("WARNING - Config problem on option : " + option + " , info is : \n" + err, "#3498db"));
+                    break;
 
-        var errConfig = function(err,option) {
-            console.error(kuler("WARNING - Config problem on option : " + option + " , error is : \n" + err , "red"))
+                case "valid":
+                    console.log(kuler("SUCCESS - " + option + " , on  : \n" + err, "#2ecc71"));
+                    break;
+            }
         };
+
+        var getContent = function(key,path){
+            var content ;
+            if(key === "pathPertinence"){
+                content = objectPath.get(path);
+            }
+            if(key === "pathSilence"){
+                content = objectPath.get(path);
+            }
+        };
+
+        /**
+         * Check if option is send
+         * @param option is the option to check
+         * @returns {boolean}
+         */
+        var check = function(option){
+            if(!config[option]){
+                infos("L'option n'a pas été précisée","error",option);
+                process.exit(1);
+            }
+            return true;
+        };
+
+        if(check("pathPertinence") && check("pathSilence")){
+
+        }
+
+
+
+
+
         submit(null, input);
     }
 };
--- loaders/tei-format/version.js
+++ loaders/tei-format/version.js
@@ -2,3 +2,11 @@
  * Created by matthias on 01/11/14.
  * This Script only call the correct format js file (via config option)
  */
+
+/*var check = function(option){
+    switch (option){
+        case ("pathSilence"):
+            return "good";
+            break;
+    }
+};*/
