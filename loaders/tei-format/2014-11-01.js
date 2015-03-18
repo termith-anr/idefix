8021ca63e11ffae26ea6d3ce207d6713a9b29143
MatthD <matthias.dieudonne@gmail.com>
2015-03-18 09:22:52 +0100

    Improvement to JSONSelect Phase1

--- loaders/tei-format/2014-11-01.js
+++ loaders/tei-format/2014-11-01.js
@@ -2,13 +2,18 @@
  * Created by matthias on 01/11/201.
  * VERSION: PHASE 1 (2014-11-01)
  */
+
+
 /************************
  **** MODULES ****
  ************************/
+
+
 var objectPath = require('object-path'),
     kuler = require('kuler'),
     sha1 = require('sha1'),
     jsonselect = require('JSONSelect');
+
 
 'use strict';
 module.exports = function(options, config) {
@@ -16,62 +21,79 @@ module.exports = function(options, config) {
     config = config.get();
     return function (input, submit) {
 
+
+        /************************
+         ****   EXECUTION    ****
+         ************************/
+
+
         /*
-        * pertinencesNames  is an array of methods names , insert in input
-        * keywords  is an array of  words , insert in input
-        * */
-        var pertinencesNames = [],
-            keywords = [],
-            arr = [];
+         * pertinencesNames  is an array of methods names , insert in input
+         * keywords is an array of  words , insert in input
+         * */
+        var pertinencesNames, keywords = [];
 
-        jsonselect.match(".keywords" , input.content.json , function(element){
+        jsonselect.forEach(".keywords:only-child" , input.content.json , function(element){
 
+            // return only pertinences
             var pertinences = element.filter(function(content){
                 return ((content.scheme !== "inist-francis" ) && (content.scheme !== "inist-pascal" ) && (content.scheme !== "cc" ) && (content.scheme !== "author" ) && (content['xml#lang'] == "fr" ) );
             });
+            // return only silences
             var silences = element.filter(function(content){
                 return (((content.scheme == "inist-francis" ) || (content.scheme == "inist-pascal" )) && ((content['xml#lang'] == "fr" )));
             });
 
-            /*
-            * PERTINENCES ACTIONS
-            * */
             pertinencesNames = jsonselect.match(".scheme" , pertinences);
 
-            //console.log("pertinencesNames : " , pertinencesNames);
-
+            /*
+             * Create pertinences objects
+             * */
             jsonselect.forEach(":has(:root > .term)" , pertinences ,function(terms){
+
+                //current pertinence method
                 var methodName  =  terms.scheme;
 
+                // For every #text keywords
                 jsonselect.forEach(".#text" , terms , function(word){
+
+                    // Create uniq id
                     var id = sha1("pertinence"+methodName+word);
-                    //console.log(" id " , id , " word ", word);
+
+                    //Create object word
                     var obj = {
-                      "id" : id,
-                      "type" : "pertinence",
-                      "method" : methodName,
-                      "word" : word
+                        "id" : id,
+                        "type" : "pertinence",
+                        "method" : methodName,
+                        "word" : word
                     };
 
-                    arr.push(obj);
+                    // Add object to keywords master array
+                    keywords.push(obj);
 
                 });
-                //console.log(arr);
+
             });
 
             /*
-             * SILENCES ACTIONS
+             * Create Silence Objects
              * */
 
             jsonselect.forEach(":has(:root > .term)" , silences ,function(terms){
 
+                //For every pertinences methods name :
+                // multiply X silences word BY  Y number of pertinence methods
                 for(var i = 0  ; i < pertinencesNames.length ; i++) {
 
+                    // Current pertinence method
                     var methodName = pertinencesNames[i];
 
+                    // For every #text keywords
                     jsonselect.forEach(".#text", terms, function (word) {
+
+                        //Create uniq ID
                         var id = sha1("silence" + methodName + word);
-                        console.log(" id " , id , " word ", word);
+
                         var obj = {
                             "id": id,
                             "type": "silence",
@@ -79,175 +101,21 @@ module.exports = function(options, config) {
                             "word": word
                         };
 
-                        arr.push(obj);
+                        // Add object to keywords master array
+                        keywords.push(obj);
 
                     });
 
                 }
-                console.log(arr);
             });
 
         });
 
-        objectPath.ensureExists(input, "keywords" , arr);
+        // ADD Keywords master array & pertinences methods names to input
+        objectPath.ensureExists(input, "keywords" , keywords);
         objectPath.ensureExists(input, "pertinenceMethods" , pertinencesNames);
 
-        /***********************
-         **** FUNCTIONS ****
-         ************************/
-        /**
-         * Infos show any kind of informations in console about the configuration
-         * @param err STRING the message to show
-         * @param type STRING (error , warning , info , valid)
-         * @param option STRING the option concerned
-         */
-        var infos = function(err,type,option) {
-            switch(type) {
-                case "error":
-                    console.error(kuler("ERROR - Config fails on option : " + option + " - Error is : \n" + err, "#e67e22"));
-                    break;
-                case "warning":
-                    console.warn(kuler("WARNING - Config problem on option : " + option + " , warning is : \n" + err, "#f39c12"));
-                    break;
-                case "info":
-                    console.info(kuler("WARNING - Config problem on option : " + option + " , info is : \n" + err, "#3498db"));
-                    break;
-                case "valid":
-                    console.log(kuler("SUCCESS - " + option + " , on : \n" + err, "#2ecc71"));
-                    break;
-            }
-        };
-        /**
-         * Check if option is send
-         * @param option {STRING} is the option to check
-         * @param type {STRING}(config/options) what to check?
-         * @returns {boolean}
-         */
-        var check = function(option,type){
-            if(type === "config"){
-                if(typeof(config[option]) == "undefined"){
-                    infos("L'option générale n'a pas été précisée","error",option);
-                    process.exit(1);
-                }
-            }
-            if(type === "options"){
-                if(typeof(options[option]) == "undefined"){
-                    infos("L'option du loader n'a pas été précisée","error",option);
-                    process.exit(1);
-                }
-            }
-            return true;
-        };
-        /**
-         * getContent of input
-         * @param key {STRING} what to get
-         * @param path {STRING} path where to get
-         * @returns {*}
-         */
-        var getContent = function(key,path){
-            var content;
-            path = "content.json." +path;
-            if(key === "pertinence"){
-                content = objectPath.get(input , path);
-            }
-            if(key === "silence"){
-                content = objectPath.get(input ,path);
-            }
-            return content;
-        };
-        /***
-         * filterContent of array to keep usefull
-         * @param content {ARRAY}
-         * @param type {STRING} what ? pertinence /silence
-         * @returns {*}
-         */
-        var filterContent = function(content , type){
-            if(type === "pertinence") {
-                return content.filter(function(content){
-                    return ((content.scheme !== "inist-francis" ) && (content.scheme !== "inist-pascal" ) && (content.scheme !== "cc" ) && (content.scheme !== "author" ) && (content['xml#lang'] == "fr" ) );
-                });
-            }
-            if(type === "silence"){
-                return content.filter(function(content) {
-                    return (((content.scheme == "inist-francis" ) || (content.scheme == "inist-pascal" )) && ((content['xml#lang'] == "fr" )));
-                });
-            }
-        };
-        /**
-         * getMethodsNames of pertinences document
-         * @param content {ARRAY} the array containing brut pertinence
-         * @returns {Array}
-         */
-        var getMethodsNames = function(content){
-            var arr = [];
-            for(var i=0 ; i < content.length ; i++){
-                arr.push(content[i].scheme);
-            }
-            return arr;
-        };
-        /**
-         * formContent , build an array of object words , the formm for keywords database
-         * @param content {ARRAY} an array of pertinence or silence keywords
-         * @param type {STRING} specify silence/pertinence type
-         * @param methodsName {ARRAY} an array of methods pertinence names
-         * @returns {Array}
-         */
-        var formContent = function(content,type,methodsName){
-            var arr = [];
-            if(type === "pertinence") {
-//console.log('content ' , content);
-                for(var i= 0 ; i < content.length ; i++){
-                    var methodName = content[i].scheme.toString();
-//console.log(methodName);
-                    for(var key in content[i].term){
-                        var word = content[i].term[key]['#text'].toString(),
-                            id = sha1(type+methodName+word),
-                            objectWord = {id : id , type : type , method : methodName , word : word};
-//console.log(objectWord);
-                        arr.push(objectWord);
-                    }
-                }
-                return arr;
-            }
-            if(type === "silence") {
-                for(var i = 0 ; i < content[0].term.length ; i++){ // Pour chaque mot
-//console.log(' MOT N° : ' , i);
-                    for(var j= 0 ; j < methodsName.length ; j++) { // Pour chaque méthode
-//console.log(' METHODE N° ' , j );
-                        var word = content[0].term[i]['#text'].toString(),
-                            id = sha1(type + methodsName[j] + word),
-                            objectWord = {id: id, type: type, method: methodsName[j], word: word};
-                        arr.push(objectWord);
-                    }
-                }
-                return arr;
-            }
-        };
-        /**
-         * insertContent in the flux to mongo then
-         * @param content {*} what to insert
-         * @param path {STRING} where to insert
-         */
-        var insertContent = function(content , path){
-            objectPath.ensureExists(input, path, content);
-        };
-        /************************
-         **** EXECUTION ****
-         ************************/
-        /*if(check("pathPertinence","config") && check("pathSilence","config")){
-            var pertinence = getContent("pertinence", config.pathPertinence),
-                silence = getContent("silence", config.pathSilence);
-            silence = filterContent(silence,"silence");
-            pertinence = filterContent(pertinence,"pertinence");
-            var arrPertinence = formContent(pertinence, "pertinence"),
-                pertinenceMethods = getMethodsNames(pertinence),
-                arrSilence = formContent(silence, "silence" , pertinenceMethods),
-                listOfKeywords = arrPertinence.concat(arrSilence);
-//console.log(listOfKeywords);
-//console.log("--------------------------------\n");
-            insertContent(listOfKeywords,"keywords");
-            insertContent(pertinenceMethods,"pertinenceMethods");
-        }*/
+
         /************************
          **** NEXT LOADER ****
          ************************/
